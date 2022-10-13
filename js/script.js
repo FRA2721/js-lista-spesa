80a58835ace8b611a9f68f39fd0d40cf7d7e3dc4
FRA2721 <cimino.francesco36@gmail.com>
2022-10-13 16:50:39 +0200

    set and print list items

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
 // programmer: Francesco Cimino;
 // project: Lista della spesa con ciclo while;
 // language: javascript;
+
+
+
+// list-items
+const listItems = ["Milk;", "Water;", "Bread;", "Orange juice;", "Blackberry;", "Pasta;", "Eggs;"];
+const shoppingList = document.getElementById("shopping-list");
+
+let i = 0;
+
+// loop
+while (i < listItems.length) {
+    let item = `<li>${listItems[i]}</li>`
+    shoppingList.innerHTML += item;
+    i++;  
+}
